93e876f47542e159c64d37ed974ac9b90ab507d1
gabrielmendes64 <gabriel10010100@gmail.com>
2025-01-10 20:39:43 -0300

    feat: add CrudApp project

---
+++ crudapp/app/src/main/java/com/gabriel/crudapp/User.kt
@@ -0,0 +1,10 @@
+package com.gabriel.crudapp
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User (
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val age: Int
+)
